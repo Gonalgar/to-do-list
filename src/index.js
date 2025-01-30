08206a1f913cb9f787331498a01d02990d6aaece
Gonalgar <gonzalo.alvarez@alumnos.upm.es>
2025-01-30 13:21:52 +0100

    Add New Project working Dialog

--- src/index.js
+++ src/index.js
@@ -1,24 +1,48 @@
 import "./style.css"
 import { Project, Task } from "./tasks"
-import { addProjectUI, addTaskUI } from "./ui"
+import { addProjectUI, addTaskUI, populateProjectsUI, populateTasksUI } from "./ui"
+import { loadData, saveData } from "./storage"
 
-const project = new Project("Default Project");
-const task = new Task("Default Task", "Default Description", "2026-01-01", "High");
+const projects = loadData();
+
+
+if (projects.length == 0) {
+    const project = new Project("Home");
+    const task = new Task("Task Example", "This is the description of the task", "2026-01-01", "High");
+    project.addTask(task);
+    projects.push(project);
+    saveData(projects);
+}
+
+populateProjectsUI(projects);
+populateTasksUI(projects[0].tasks);
+
 
 const addProjectButton = document.querySelector(".menu-item.add-project");
-const addTaskButton = document.querySelector(".add-task");
 
+const ProjectDialog = document.querySelector(".add-project-dialog");
 addProjectButton.addEventListener("click", () => {
-    const projectName = prompt("Enter the project name");
-    const project = new Project(projectName);
-    addProjectUI(project);
+    ProjectDialog.showModal();
 });
 
+const cancelProjectButton = document.querySelector(".cancel-project-btn");
+cancelProjectButton.addEventListener("click", () => {
+    ProjectDialog.close();
+});
+
+
+ProjectDialog.querySelector(".create-project-btn").addEventListener("click", () => {
+    const projectName = ProjectDialog.querySelector(".project-name").value;
+    const project = new Project(projectName);
+    projects.push(project);
+    addProjectUI(project);
+    saveData(projects);
+    ProjectDialog.close();
+});
+
+
+const addTaskButton = document.querySelector(".add-task");
+const taskDialog = document.querySelector(".add-task-dialog");
 addTaskButton.addEventListener("click", () => {
-    const taskName = prompt("Enter the task name");
-    const taskDescription = prompt("Enter the task description");
-    const taskDueDate = prompt("Enter the task due date");
-    const taskPriority = prompt("Enter the task priority");
-    const task = new Task(taskName, taskDescription, taskDueDate, taskPriority);
-    addTaskUI(task);
+    taskDialog.showModal();
 });

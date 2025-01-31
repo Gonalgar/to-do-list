347091e0c4cf155a5c2223273b15e7e940ec447a
Gonalgar <gonzalo.alvarez@alumnos.upm.es>
2025-01-31 10:40:42 +0100

    loadData parse to Project objects

--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,5 @@
+import { Project, Task } from './tasks';
+
 const STORAGE_KEY = 'DonezoData';
 
 function saveData(projects) {
@@ -6,7 +8,18 @@ function saveData(projects) {
 
 function loadData() {
     const data = localStorage.getItem(STORAGE_KEY);
-    return data ? JSON.parse(data) : [];
+    const parsedData = data ? JSON.parse(data) : [];
+    return parsedData.map(projectData => {
+        const project = new Project(projectData._title);
+        project.id = projectData.id;
+        project.tasks = projectData.tasks.map(taskData => {
+            const task = new Task(taskData._title, taskData._description, taskData._dueDate, taskData._priority);
+            task.id = taskData.id;
+            task.done = taskData._done;
+            return task;
+        });
+        return project;
+    });
 }
 
 export { saveData, loadData };
